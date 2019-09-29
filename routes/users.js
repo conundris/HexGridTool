03f6e97d32b6a46fc22400d8b2adafa938f10299
maniacalbrain <maniacalcorp@gmail.com>
2019-09-29 15:54:54 +0100

    Installed MongoDB, edited route to read from collection

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	var MongoClient = require('mongodb').MongoClient;
+	var url = "mongodb://localhost:27017/";
+	
+	MongoClient.connect(url, function(err, db) {
+		if(err) throw err;
+		var dbo = db.db("mtg");
+		dbo.collection("TimeSpiral").findOne({}, function(err, result){
+			if(err) throw err;			
+			console.log(result);
+			db.close();
+		});
+	});	
+	res.send('respond with a resource!!!');
 });
 
 module.exports = router;
