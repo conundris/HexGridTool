b773fb040c5cae8e00581d7a2e345e2a9c71b090
Jason Brockett <jason.h.brockett@gmail.com>
2019-09-30 23:52:48 +0200

    Added swagger stats first pass

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var specLocation = path.join(__dirname, 'swagger.json');
+const swStats = require('swagger-stats');
+var swaggerSpec = require( specLocation );
+//const expressSwagger = require('express-swagger-generator')(app);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(swStats.getMiddleware({swaggerSpec:swaggerSpec}));
 
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/stylesheets'));
