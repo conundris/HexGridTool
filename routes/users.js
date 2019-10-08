0dd3c9d6b2785db1d1ccd588c0eebb4d13d9be38
maniacalbrain <maniacalcorp@gmail.com>
2019-10-08 14:30:37 +0100

    Wrote POST route

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var url = "mongodb://localhost:27017/";
+
+	
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	var MongoClient = require('mongodb').MongoClient;
-	var url = "mongodb://localhost:27017/";
-	
+		
 	MongoClient.connect(url, function(err, db) {
 		if(err) throw err;
-		var dbo = db.db("mtg");
-		dbo.collection("TimeSpiral").findOne({}, function(err, result){
+		var dbo = db.db("dndTest");
+		dbo.collection("chult").findOne({}, function(err, result){
 			if(err) throw err;			
 			console.log(result);
 			db.close();
 		});
 	});	
-	res.send('respond with a resource!!!');
+	//res.send('respond with a resource!!!');
+});
+
+
+router.post('/', function(req, res, next) {
+	var MongoClient = require('mongodb').MongoClient;
+		
+	MongoClient.connect(url, function(err, db) {
+		if(err) throw err;
+		var dbo = db.db("dndTest");
+		dbo.collection("chult").insertOne(req.body, function(err, result){
+			if(err) throw err;			
+			console.log(result);
+			db.close();
+		});
+	});	
 });
 
 module.exports = router;
